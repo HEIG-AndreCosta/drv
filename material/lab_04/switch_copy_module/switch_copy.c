439b13556d851babca755f82a3f9cf3d5886157f
Clement Dieperink <clement.dieperink@heig-vd.ch>
2024-04-17 08:50:11 +0200

    add lab04

---
+++ material/lab_04/switch_copy_module/switch_copy.c
@@ -0,0 +1,51 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/sched.h>
+#include <linux/platform_device.h>
+#include <linux/io.h>
+#include <linux/ioport.h>
+#include <linux/of.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("REDS");
+MODULE_DESCRIPTION("Introduction to the interrupt and platform drivers");
+
+struct priv {};
+
+static irqreturn_t irq_handler(int irq, void *dev_id)
+{
+	return IRQ_HANDLED;
+}
+
+static int switch_copy_probe(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static int switch_copy_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static const struct of_device_id switch_copy_driver_id[] = {
+	{ .compatible = "drv2024" },
+	{ /* END */ },
+};
+
+MODULE_DEVICE_TABLE(of, switch_copy_driver_id);
+
+static struct platform_driver switch_copy_driver = {
+	.driver = {
+		.name = "drv-lab4",
+		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(switch_copy_driver_id),
+	},
+	.probe = switch_copy_probe,
+	.remove = switch_copy_remove,
+};
+
+module_platform_driver(switch_copy_driver);
